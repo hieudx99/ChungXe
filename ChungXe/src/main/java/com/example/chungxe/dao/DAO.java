c7bd48817d7bd56716aaf3454b0a890c1ad65469
hieudx99 <hieudx1104@gmail.com>
2022-03-18 14:45:29 +0700

    update: jdbc driver

--- ChungXe/src/main/java/com/example/chungxe/dao/DAO.java
+++ ChungXe/src/main/java/com/example/chungxe/dao/DAO.java
@@ -9,10 +9,10 @@ public class DAO {
     public DAO() {
         if (con == null) {
             String dbUrl = "jdbc:mysql://127.0.0.1:3306/sqa?useSSL=false";
-            String dbClass = "com.mysql.jdbc.Driver";
+            String dbClass = "com.mysql.cj.jdbc.Driver";
             try {
                 Class.forName(dbClass);
-                con = DriverManager.getConnection(dbUrl, "root", "Khai15102@");
+                con = DriverManager.getConnection(dbUrl, "root", "b18dcdt073");
             } catch (Exception e) {
                 e.printStackTrace();
             }
