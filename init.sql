8f58a4ce10c82163ba688e8356f6765886d43b23
hieudx99 <hieudx1104@gmail.com>
2022-03-21 22:35:59 +0700

    update: tblCar add column price

--- init.sql
+++ init.sql
@@ -42,10 +42,11 @@ CREATE TABLE tblCar (
     color varchar(255) NOT NULL, 
     licensePlate varchar(255) NOT NULL UNIQUE, 
     seatNumber int NOT NULL, 
-    image varchar(255) NOT NULL, 
-    status varchar(255) NOT NULL, 
-    categoryId int NOT NULL, 
-    branchId int NOT NULL, 
+    price float NOT NULL,
+    image varchar(255) NOT NULL,
+    status varchar(255) NOT NULL,
+    categoryId int NOT NULL,
+    branchId int NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (branchId) references tblBranch(id),
     FOREIGN KEY (categoryId) references tblCarCategory(id)
@@ -91,10 +92,38 @@ insert into tblBranch(name) values ('Honda');
 insert into tblBranch(name) values ('Vinfast');
 
 
-insert into tblCar(name, color, licensePlate, seatNumber, image, status, categoryId, branchId) 
-values ('VINFAST LUX  A2.0', 'Brahminy White', '30A-686.86', 4 , 
+insert into tblCar(name, color, licensePlate, seatNumber, price, image, status, categoryId, branchId) 
+values ('VINFAST LUX  A2.0', 'Brahminy White', '30A-686.86', 4, 600000, 
 'https://shop.vinfastauto.com/on/demandware.static/-/Sites-app_vinfast_vn-Library/default/dw218d05cc/images/Lux-A/hinh-anh-gia-VinFast-Lux-a2.0-price-mau-trang-brahminy-white.png',
- 'A', 3 , 5);
+ 'A', 3, 5);
+ 
+insert into tblCar(name, color, licensePlate, seatNumber, price, image, status, categoryId, branchId) 
+values ('Honda City', 'Red', '30A-555.55', 4, 500000, 
+'https://i.xeoto.com.vn/auto/honda/city/honda-city-2021.png',
+ 'A', 2, 4);
+ 
+insert into tblCar(name, color, licensePlate, seatNumber, price, image, status, categoryId, branchId) 
+values ('Toyota Camry', 'White', '30A-666.66', 5, 700000, 
+'https://cdn1.otosaigon.com/data-resize/carinfo/20210727/EZ03OyWCbnG8t4d1Su88pZKOZpOgoSpXiFRu3uxP.jpg?w=838',
+ 'A', 4, 3);
+ 
+ insert into tblCar(name, color, licensePlate, seatNumber, price, image, status, categoryId, branchId) 
+values ('Toyota Fortuner', 'White', '30A-777.77', 7, 900000, 
+'https://i.xeoto.com.vn/auto/w600/toyota/fortuner/toyota-fortuner-2020-52327.png',
+ 'A', 4, 3);
+ 
+ insert into tblCar(name, color, licensePlate, seatNumber, price, image, status, categoryId, branchId) 
+values ('VINFAST FADIL', 'Luxury Blue', '30A-444.44', 5, 500000, 
+'https://shop.vinfastauto.com/on/demandware.static/-/Sites-vinfast_vn_master/default/dw02ef2e69/images/FADIL/SMA/CE18.png',
+ 'A', 1, 5);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
 
 
