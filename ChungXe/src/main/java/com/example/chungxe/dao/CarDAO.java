4ac51110deb05ec9077d0e3647bbde6297e662f4
hieudx99 <hieudx1104@gmail.com>
2022-03-21 09:38:08 +0700

    add: function search car

--- ChungXe/src/main/java/com/example/chungxe/dao/CarDAO.java
+++ ChungXe/src/main/java/com/example/chungxe/dao/CarDAO.java
@@ -8,6 +8,7 @@ import java.util.List;
 public interface CarDAO {
     public List<Car> getListCar();
     public Car getCarByID(int carID);
+    public List<Car> searchCar(String kw, int nbrSeat, int branchId, int categoryId);
     public List<Statistic> getStatisticByCar(String startDate, String endDate);
 
 }
